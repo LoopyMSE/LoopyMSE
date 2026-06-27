d0ef78fb62769cd5e1e5d7ca2a311c739ed9b362
bobsayshilol <bobsayshilol@live.co.uk>
2026-06-27 01:29:08 +0100

    Plug remaining leaks
    SDL_GetBasePath() and SDL_GetPrefPath() both return allocated strings,
    so deallocate them after we're done with them.

--- src/sdl/config.h
+++ src/sdl/config.h
@@ -12,15 +12,22 @@
 #endif
 
 namespace fs = std::filesystem;
+
+static fs::path path_from_sdl(char *sdl_path) {
+	fs::path path(sdl_path);
+	SDL_free(sdl_path);
+	return path;
+}
+
 const fs::path DEFAULT_BIOS_PATH = "bios.bin";
 const fs::path DEFAULT_SOUND_BIOS_PATH = "soundbios.bin";
 const fs::path CONTROLLER_DB_PATH = "gamecontrollerdb.txt";
 const fs::path INI_PATH = "loopymse.ini";
-const fs::path RESOURCE_PATH = SDL_GetBasePath();
+const fs::path RESOURCE_PATH = path_from_sdl(SDL_GetBasePath());
 const fs::path PREFS_PATH = 
 #ifdef _WIN32
 // Don't use %APPDATA% on windows, keep everything beside .exe
-SDL_GetBasePath();
+path_from_sdl(SDL_GetBasePath());
 #else
-SDL_GetPrefPath(PROJECT_ORG, PROJECT_NAME);
+path_from_sdl(SDL_GetPrefPath(PROJECT_ORG, PROJECT_NAME));
 #endif
